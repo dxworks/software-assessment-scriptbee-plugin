2d229909c3e3091894e059e0def22447dfbb64d1
Andrei Timar <atimar999@gmail.com>
2022-12-17 23:15:03 +0200

    Added Loaders in constructor to get the name

--- DxWorks.ScriptBee.Plugins.SoftwareAssessment/SoftwareAssessmentLinker.cs
+++ DxWorks.ScriptBee.Plugins.SoftwareAssessment/SoftwareAssessmentLinker.cs
@@ -3,19 +3,34 @@ using DxWorks.ScriptBee.Plugins.Honeydew;
 using DxWorks.ScriptBee.Plugins.Honeydew.Models;
 using Dxworks.ScriptBee.Plugins.InspectorGit.Model;
 using DxWorks.ScriptBee.Plugins.JiraMiner.Models;
+using InspectorGitLoader = Dxworks.ScriptBee.Plugins.InspectorGit.ScriptBeeModelLoader;
+using JiraMinerLoader = DxWorks.ScriptBee.Plugins.JiraMiner.Loaders.ModelLoader;
+using HoneydewLoader = DxWorks.ScriptBee.Plugins.Honeydew.Loaders.ModelLoader;
 
 namespace DxWorks.ScriptBee.Plugins.SoftwareAssessment;
 
 public class SoftwareAssessmentLinker : IModelLinker
 {
+    private readonly string _inspectorGitPluginName;
+    private readonly string _jiraMinerPluginName;
+    private readonly string _honeydewPluginName;
+
+    public SoftwareAssessmentLinker(IEnumerable<IModelLoader> loaders)
+    {
+        var modelLoaders = loaders.ToList();
+        _inspectorGitPluginName = modelLoaders.OfType<InspectorGitLoader>().First().GetName();
+        _jiraMinerPluginName = modelLoaders.OfType<JiraMinerLoader>().First().GetName();
+        _honeydewPluginName = modelLoaders.OfType<HoneydewLoader>().First().GetName();
+    }
+
     public string GetName() => "software-assessment";
 
     public Task LinkModel(Dictionary<Tuple<string, string>, Dictionary<string, ScriptBeeModel>> context,
         Dictionary<string, object>? configuration = default, CancellationToken cancellationToken = default)
     {
-        var repos = context[Tuple.Create("Repository", "InspectorGit")].Values.Cast<Repository>();
-        var issues = context[Tuple.Create("Issue", "jira")].Values.Cast<Issue>();
-        var files = context[Tuple.Create("File", "honeydew")].Values.Cast<FileModel>();
+        var repos = context[Tuple.Create("Repository", _inspectorGitPluginName)].Values.Cast<Repository>();
+        var issues = context[Tuple.Create("Issue", _jiraMinerPluginName)].Values.Cast<Issue>();
+        var files = context[Tuple.Create("File", _honeydewPluginName)].Values.Cast<FileModel>();
 
         foreach (var issue in issues)
         {
